57c04e08e5d6637faf919c7f1d5d31abc7a50642
1Sara19 <s202245780@kfupm.edu.sa>
2026-03-04 11:17:50 +0300

    TODO2

--- 5-6-react-styling-fetch-data/src/App.jsx
+++ 5-6-react-styling-fetch-data/src/App.jsx
@@ -204,6 +204,29 @@ export default function App() {
      ========================================================= */
   useEffect(() => {
     // TODO 2.1: Implement fetching users here (see lab instructions)
+    const fetchUsers = async () => {
+      try {
+         setLoading(true);
+         setError(null);
+
+         const response = await fetch("https://jsonplaceholder.typicode.com/users");
+
+         if (!response.ok) {
+         throw new Error("Failed to fetch users");
+         }
+
+         const data = await response.json();
+
+         setUsers(data);
+         setFilteredUsers(data);
+      } catch (err) {
+         setError(err.message);
+      } finally {
+         setLoading(false);
+      }
+   };
+
+   fetchUsers();
   }, []);
 
   /* =========================================================
@@ -215,6 +238,14 @@ export default function App() {
      ========================================================= */
   useEffect(() => {
     // TODO 2.2: Implement filtering users here (see lab instructions)
+   if (!searchTerm) {
+      setFilteredUsers(users);
+   } else {
+      const filtered = users.filter((user) =>
+         user.name.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+      setFilteredUsers(filtered);
+   }
   }, [searchTerm, users]);
 
   // Modal handlers (already complete)
@@ -252,7 +283,7 @@ export default function App() {
 
         <UserModal show={showModal} user={selectedUser} onHide={handleCloseModal} />
       </Container>
-      
+
       {/* TODO 1.1: Set footer className EXACTLY as in lab instructions */}
       <footer className="bg-light py-4 mt-5">
         <Container>
